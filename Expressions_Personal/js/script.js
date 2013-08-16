80d01e02b95ff753bf697e1455ecb2bda5699146
Shannon Ewing-Sudds <ewing79@fullsail.edu>
2013-08-16 18:00:15 -0500

    Expressions_Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,9 @@
 alert("This is in the js file");
+//Shannon Ewing-Sudds 16 Aug 2013 Expressions_Personal
+//For the personal calculation I am going to calculate how many shoes I buy in a year by calculating how many shoes i buy in a month
+var year = 365 
+var month = 30
+var shoes = function (number){
+  var val = (number * 30);
+  console.log(val)
+};
