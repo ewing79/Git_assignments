c64c879ecc70181304ffc2dbcaf4d7b58ead7766
ewing79 <ewing79@fullsail.edu>
2013-08-15 21:05:38 -0500

    add files to git repository

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,48 @@
+//EwingSudds, Shannon 15 August 2013 Expressions_Assignment
+alert("This is in the js file");
+//Expressions_Industry
+//This will be a calculator to determine if I passed my Physical Training Test at work
+var age = prompt("what's your age?");
+if (age < 17)
+    console.log("You can not take the Amry APFT at this time");
+else
+{
+    console.log("Good Luck on your test scores!");
+}
+var pushUps = prompt("how many push ups did you do?");
+if (pushUps < 15)
+    console.log("You didn't pass your push ups");
+else
+{
+    console.log("Congratulations you passed your push ups!");
+}
+var sitUps = prompt("how many sit ups did you do?");
+if (sitUps < 42)
+    console.log("You didn't pass your sit ups");
+else
+{
+    console.log("Congratulations you passes your sit ups!");
+}
+var runTime = prompt("what was your run time?");
+if (runTime < 2142)
+    console.log("You didn't pass your runt time");
+else
+{
+    console.log("Congratulations you passed your run time!");
+}
+var events = prompt("how many events did you pass?");
+if (events < 3)
+    console.log("You didn't pass your Physical Fitness Test");
+else
+{
+    console.log("Congratulations you passes your Physical Fitness Test!")
+}
+
+
+
+
+
+
+
+
+
---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,10 @@
+//Shannon Ewing-Sudds 13 Aug 2013 Expressions_Worksheet
+//Dog Years
+var humanAge = function (number) {
+    var sparkyAge = number * 7;
+    console.log(sparkyAge);
+};
+humanAge(3);
+
+
+
