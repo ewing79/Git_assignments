dc452002bad3a34742fab80e278f1e53eef17a0d
ewing79 <ewing79@fullsail.edu>
2013-08-16 20:03:47 -0500

    update Expressions_Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,14 +4,13 @@
 //This will be a calculator to determine how many days I would have shopped
 // //for shoes and how many shoes I would have bought in 1 year depending on how many shoes I buy per month
 alert("This is in the js file");
-//unit in days
 var days = 1;
 var daysInAWeek =  7;
 var week = days * 7;
 var weeksInAMonth = 4;
 var daysInAMonth = week * weeksInAMonth;
 var monthsInAYear = 12;
-var daysInAyear = monthsInAYear * daysInAMonth;
+var daysInAYear = monthsInAYear * daysInAMonth;
 var shoesPerYear = monthsInAYear * prompt("how many shoes do you buy in a month");
 console.log(shoesPerYear);
 
