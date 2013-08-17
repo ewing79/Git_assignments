232aa3c7c5bc3181f0f83aedced58c7bab3958c4
ewing79 <ewing79@fullsail.edu>
2013-08-16 20:51:26 -0500

    Expressions_Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,7 +5,7 @@ alert("Expressions Assingment_Wacky")
 //licks it took them to get to the center
 alert("Lick the lollipop for one minute and count how many times you licked the lollipop in one miute");
 var minutes = 1;
-var minutesInAnHour = mintues * 60;
+var minutesInAnHour = minutes * 60;
 var licksPerMinute = prompt("How many licks did you lick per minute?")
 var mintuesToLick= prompt("how many mintues did it take you to get to the center of the lollipop?");
 console.log(mintuesToLick / licksPerMinute)
