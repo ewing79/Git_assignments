6698087b63f6967c626e344adbc7774376c750cb
ewing79 <ewing79@fullsail.edu>
2013-08-16 20:04:58 -0500

    update Expressions_Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,12 +5,10 @@
 // //for shoes and how many shoes I would have bought in 1 year depending on how many shoes I buy per month
 alert("This is in the js file");
 var days = 1;
-var daysInAWeek =  7;
 var week = days * 7;
 var weeksInAMonth = 4;
 var daysInAMonth = week * weeksInAMonth;
 var monthsInAYear = 12;
-var daysInAYear = monthsInAYear * daysInAMonth;
 var shoesPerYear = monthsInAYear * prompt("how many shoes do you buy in a month");
 console.log(shoesPerYear);
 
