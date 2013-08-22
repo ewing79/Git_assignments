c65b1709fa347bd8ca336833b4bb741c1bfa942b
ewing79 <ewing79@fullsail.edu>
2013-08-21 19:49:50 -0500

    update files

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,8 @@
+//Shannon Ewing-Sudds 21 Aug 2013 Conditionals_Wacky
+//Has SpongeBob cooked enough Crabby Patties to feed all the customers.
+//Their are 100 customers in the Crusty Crab. SpongeBob needs to figure out if
+//he has cooked enough patties or how many more does he need to cook.
+//First I will list the givens
+//There are 100 sea creatures at the Crusty Crab
+var customers = 100;
+//How many patties has SpngeBob cooked?
