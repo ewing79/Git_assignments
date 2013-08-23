5780c6d873237fca7d5fda90f41e57905d85539a
Shannon Ewing-Sudds <ewing79@fullsail.edu>
2013-08-22 21:23:57 -0500

    Update script.js
    final Committ

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,18 +2,17 @@
 //In this assignment I will decide if I have completed my ruck 
 //march in enough time.
 //First I will list the givens
-var ruckWeight = prompt("what is your ruck wight");
-//ruck weight has to be at least 30lbs
-var milesCompleted = Prompt("How many miles did you complete");
-//must complete at least 8 miles
-var timeCompleted = prompt("how long did it take you to complete the ruck march?");
-//Ruck March must be completed in 2 hours
-var passWeight = 30;
+//Shannon Ewing-Sudds 22 Aug 2013 Conditionals_worksheet
+//In this assignment I will decide if I have completed my ruck 
+//march in enough time.
+//First I will list the givens
+var ruckWeight = 28;
 //weight in lbs
-var passMiles = 8;
+var milesCompleted = 8;
 //must ruck march 8 miles
-var passTime = 2;
+var timeCompleted = 2;
 //must complete ruck march in 2 hours
+
 if(ruckWeight != 30)
 {
     console.log("You didn't have enough weight");
@@ -25,4 +24,8 @@ if(milesCompleted != 8)
 if(timeCompleted != 2)
 {
     console.log("You didn't finish in enough time");
+}
+if(ruckWeight >= 30 && milesCompleted ===8 && timeCompleted <= 2)
+{
+    console.log("Congratulations, You have passed the 8 mile ruck march");
 }
