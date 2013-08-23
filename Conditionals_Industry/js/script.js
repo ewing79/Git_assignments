302733adf1ca33cfb51172b72814bd832d455e9f
Shannon Ewing-Sudds <ewing79@fullsail.edu>
2013-08-22 21:02:27 -0500

    Update script.js
    update

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,3 +2,8 @@
 //In this assignment I will decide if I have completed my ruck 
 //march in enough time.
 //First I will list the givens
+var ruckWeight = prompt("what is your ruck wight");
+//ruck weight has to be at least 30lbs
+var milesCompleted = Prompt("How many miles did you complete");
+//must complete at least 8 miles
+
