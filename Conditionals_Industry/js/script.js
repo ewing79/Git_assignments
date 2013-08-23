89de626866b3adfba9c645c141c2553ff308912c
Shannon Ewing-Sudds <ewing79@fullsail.edu>
2013-08-22 21:01:39 -0500

    Update script.js
    working on conditionals_Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,4 @@
-alert("This is in the js file");
+//Shannon Ewing-Sudds 22 Aug 2013 Conditionals_worksheet
+//In this assignment I will decide if I have completed my ruck 
+//march in enough time.
+//First I will list the givens
