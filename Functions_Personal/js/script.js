ffdbd3e86ad246a3fd35d13a31f43c37b141928d
ewing79 <ewing79@fullsail.edu>
2013-08-29 18:32:46 -0500

    functions_worksheet final

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,24 +2,21 @@
 //In this assingment I will create a function that calculates how many loads of clothes
 //I have to wash in a week
 
+
 function washClothes(loads)
 {
     var daysWashed = 5;
     var washClothes = loads * daysWashed;
     console.log ("The number of loads of clothes that I have to wash a day is" + " " + washClothes);
+
+    if (washClothes <= 25){
+        console.log("That is quite a few clothes you have to wash");
+    }
+    else
+    {
+        console.log("My Goodness that is a lot of washing you have to do");
+    }
 }
-washClothes(2);
-if (washClothes <= 10)
-{
-    console.log("That's not too bad");
-}
-if (washClothes >= 20)
-{
-    console.log("That is quite a few clothes you hve to wash!!");
-}
-if (washCLothes >= 40)
-{
-    conosole.log("My Goodness That Is A Lot Of Washing!!");
-}
+washClothes(5);
 
 
