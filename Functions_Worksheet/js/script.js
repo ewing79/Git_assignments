2f63aadf61c311cd06b8056bf62ceeb801b08f4a
ewing79 <ewing79@fullsail.edu>
2013-08-29 16:44:24 -0500

    no message

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,13 @@
+//Shannon Ewing-Sudds 27 Aug 2013 Functions_Worksheet
+//Functions_Worksheet
+// Calculate the circumference of circle
+
+function circumferenceCircle(diameter)
+{
+    var circumferenceCircle = diameter * 3.14;
+    console.log ("The circumference of the circle is" + circumferenceCircle)
+}
+circumferenceCircle(2);
+circumferenceCircle(3);
+circumferenceCircle(4);
+
