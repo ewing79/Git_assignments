3436b3cbe8c3e1a79d44e076f365c44e1ca9377d
ewing79 <ewing79@fullsail.edu>
2013-08-29 17:48:43 -0500

    functions_worsksheet update

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,1 +1,2 @@
 //Shannon Ewing-Sudds 29 Aug 2013 Functions_Industry
+//Land Navigation Function
--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,22 @@
 //Shannon Ewing-Sudds 29 Aug 2013 Functions_Personal
+//In this assingment I will create a function that calculates how many loads of clothes
+//I have to wash in a week
+
+function washClothes(loads)
+{
+    var daysWashed = 5;
+    var washClothes = loads * daysWashed;
+    console.log ("The number of loads of clothes that I have to wash a day is" + " " + washClothes)
+}
+washClothes(2);
+washClothes(5);
+washClothes(10);
+if (washClothes <= 20)
+{
+   console.log("That's not too bad");
+}
+if (washClothes >= 20)
+{
+    console.log("That is quite a few clothes you hve to wash!!");
+}
+if
