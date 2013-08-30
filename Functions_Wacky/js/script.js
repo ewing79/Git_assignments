c77f1f717dcbcadafef87f0bc36d6469124c8cc3
ewing79 <ewing79@fullsail.edu>
2013-08-29 20:19:06 -0500

    functions_worksheet final

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,1 +1,21 @@
 //Shannon Ewing-Sudds 29 Aug 2013 Functions_Wacky
+//How much wood would a woodchuck chuck If a woodchuck could chuck wood?
+//He would chuck, he would, as much as he could,
+//And chuck as much as a woodchuck would. If a woodchuck could chuck wood.
+
+function chuckedWood(chucked)
+{
+    var hoursChucked = 5;
+    var chuckedWood = chucked * hoursChucked;
+    console.log ("He would chuck, he would, as much as he could, And chuck as much as a woodchuck would," + " " + chuckedWood + " " + "pounds of wood he could");
+
+    if (chuckedWood <= 25){
+        console.log("If a woodchuck could chuck wood");
+    }
+    else
+    {
+        console.log("That woodchuck chucked a whole Tree!!!");
+    }
+}
+chuckedWood(2);
+
