bfb1d1d31771a4b2f3b02ff56f4e26f4d01bc705
ewing79 <ewing79@fullsail.edu>
2013-08-29 19:51:14 -0500

    functions_worksheet final

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,2 +1,18 @@
 //Shannon Ewing-Sudds 29 Aug 2013 Functions_Industry
 //Land Navigation Function
+//I will describe how many points on the Land Navigation Course needs to be found
+
+var total = pointsFound(4);
+
+function pointsFound(p){
+    var found = p;
+    return found;
+
+}
+console.log(total);
+if(total = 4){
+    console.log("Congratulations you found all 4 points");
+}
+else{
+    console.log("You didn't find all four points")
+}
